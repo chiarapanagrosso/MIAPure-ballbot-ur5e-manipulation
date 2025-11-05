391bf7c79f80cbad7e64a2d21be8f76397cfa4c4
chiarapanagrosso <chiara.panagrosso@gmail.com>
2025-11-05 11:23:40 +0100

    Delete generated directory

--- generated/function/loadAndConfigureRobot.m
+++
@@ -1,8 +0,0 @@
-function robot = loadAndConfigureRobot(urdfPath)
-% This helper function loads a robot from a URDF and sets its DataFormat.
-% It is intended to be called as a single extrinsic function.
-
-    robot = importrobot(urdfPath);
-    robot.DataFormat = 'column';
-    
-end
